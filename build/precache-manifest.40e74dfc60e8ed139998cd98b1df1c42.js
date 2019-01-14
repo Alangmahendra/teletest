93a5baf977b722f9e28687e677664ec97bb15a2d
Alangmahendra <alangmahendra@gmail.com>
2019-01-14 10:36:54 +0700

    test

---
+++ build/precache-manifest.40e74dfc60e8ed139998cd98b1df1c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c6893f7e71081ae9080",
+    "url": "/static/js/main.0c6893f7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0c6893f7e71081ae9080",
+    "url": "/static/css/main.aec35eab.chunk.css"
+  },
+  {
+    "revision": "7e452321950e222736eeb260f0e01cef",
+    "url": "/index.html"
+  }
+];
